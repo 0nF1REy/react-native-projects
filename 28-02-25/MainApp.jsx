79d62a9529a67852a997df895a1e5d245393c0d1
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-07 04:40:52 +0000

    feat: Navigation options configuration completed

--- 28-02-25/MainApp.jsx
+++ 28-02-25/MainApp.jsx
@@ -16,9 +16,27 @@ export default function MainApp() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Notícias">
-        <Stack.Screen name="Notícias" component={Screen01} />
-        <Stack.Screen name="Primeira Noticia" component={Screen02} />
-        <Stack.Screen name="Segunda Noticia" component={Screen03} />
+        <Stack.Screen
+          name="Notícias"
+          component={Screen01}
+          options={{ title: "Notícias" }}
+        />
+        <Stack.Screen
+          name="Primeira Noticia"
+          component={Screen02}
+          options={{
+            title:
+              "Joan Jett: A Arte de Empoderar e Inspirar em Tempos de Incerteza",
+          }}
+        />
+        <Stack.Screen
+          name="Segunda Noticia"
+          component={Screen03}
+          options={{
+            title:
+              "Suzi Quatro Lança Videoclipe de 'The Devil In Me' e Anuncia Novo Álbum com Colaboração do Filho",
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
