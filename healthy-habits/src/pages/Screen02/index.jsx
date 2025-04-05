d629c9107db1159997dc07a892d6a779436fc191
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-04-05 17:18:06 -0300

    refactor,style: Refactoring and application of new styles in "Screen02" to have better organization and modularity of the code

--- healthy-habits/src/pages/Screen02/index.jsx
+++ healthy-habits/src/pages/Screen02/index.jsx
@@ -1,57 +1,72 @@
 import React from "react";
-import { Container, Input, Button, ButtonText, ResultText, BoldText, BotaoVoltarHome, TextoBotaoVoltar } from "./styles";
+import {
+  Button,
+  ButtonText,
+  ResultText,
+  BoldText,
+  BotaoVoltarHome,
+  TextoBotaoVoltar,
+  Imagem,
+  ContainerImage,
+  ScrollViewContainer,
+  ViewContainer,
+  TituloBacana,
+} from "./styles";
 import Icon from "react-native-vector-icons/Feather";
+import InputBacanaComponent from "../../components/Input";
+import { useCalcularIMC } from "../../hooks/useCalcularIMC";
 
 export default function Screen02({ navigation }) {
-  const [peso, setPeso] = React.useState("");
-  const [altura, setAltura] = React.useState("");
-  const [imc, setImc] = React.useState("");
-
-  function calcularIMC() {
-    const pesoValue = parseFloat(peso);
-    const alturaValue = parseFloat(altura) / 100;
-
-    if (!pesoValue || !alturaValue) {
-      setImc("Dados inválidos");
-      return;
-    }
-
-    const imcValue = pesoValue / (alturaValue * alturaValue);
-    setImc(imcValue.toFixed(2));
-  }
+  const { peso, altura, imc, setPeso, setAltura, calcularIMC } =
+    useCalcularIMC();
 
   function abrirTelaHome() {
     navigation.navigate("Home");
   }
 
   return (
-    <Container>
-      <Input
-        placeholder="Digite seu peso (kg), ex: 70"
-        keyboardType="numeric"
-        value={peso}
-        onChangeText={setPeso}
-      />
-      <Input
-        placeholder="Digite sua altura (cm), ex: 170"
-        keyboardType="numeric"
-        value={altura}
-        onChangeText={setAltura}
-      />
+    <ScrollViewContainer>
+      <ViewContainer>
+        <TituloBacana>Calculadora de IMC</TituloBacana>
+        <Imagem source={require("../../../assets/images/bmi-metric.jpg")} />
+        {/* COMPONENTE: Input */}
+        <InputBacanaComponent
+          placeholder="Digite seu peso (kg), ex: 70"
+          value={peso}
+          onChangeText={setPeso}
+          keyboardType="numeric"
+        />
+        <InputBacanaComponent
+          placeholder="Digite sua altura (cm), ex: 170"
+          value={altura}
+          onChangeText={setAltura}
+          keyboardType="numeric"
+        />
+        <Button onPress={calcularIMC}>
+          <ButtonText>Calcular IMC</ButtonText>
+          <Icon
+            name="activity"
+            size={20}
+            color="#fff"
+            style={{ marginLeft: 8 }}
+          />
+        </Button>
 
-      <Button onPress={calcularIMC}>
-        <ButtonText>Calcular IMC</ButtonText>
-        <Icon name="activity" size={20} color="#fff" style={{ marginLeft: 8 }} />
-      </Button>
+        <ResultText>
+          Seu IMC é: <BoldText>{imc}</BoldText>
+        </ResultText>
 
-      <ResultText>
-        Seu IMC é: <BoldText>{imc}</BoldText>
-      </ResultText>
-
-      {/* Voltar para a tela Home */}
-      <BotaoVoltarHome onPress={abrirTelaHome}>
-        <TextoBotaoVoltar>Voltar</TextoBotaoVoltar>
-      </BotaoVoltarHome>
-    </Container>
+        {/* Voltar para a tela Home com ícone */}
+        <BotaoVoltarHome onPress={abrirTelaHome}>
+          <Icon
+            name="arrow-left-circle"
+            size={20}
+            color="#fff"
+            style={{ marginRight: 8 }}
+          />
+          <TextoBotaoVoltar>Voltar</TextoBotaoVoltar>
+        </BotaoVoltarHome>
+      </ViewContainer>
+    </ScrollViewContainer>
   );
 }
--- healthy-habits/src/pages/Screen02/styles.jsx
+++ healthy-habits/src/pages/Screen02/styles.jsx
@@ -1,33 +1,35 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const ScrollViewContainer = styled.ScrollView`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #f5f5f5;
+  background-color: ${(props) => props.theme.bg};
   padding: 20px;
   font-family: "Roboto-Regular";
 `;
 
-export const Input = styled.TextInput`
-  height: 50px;
-  width: 80%;
-  margin-bottom: 15px;
-  padding-left: 10px;
-  border-radius: 8px;
-  border-width: 1px;
-  border-color: #ccc;
-  font-size: 16px;
+export const ViewContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+
+
+export const TituloBacana = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #3d85c6;
+  margin-bottom: 20px;
+  text-align: center;
 `;
 
 export const Button = styled.TouchableOpacity`
   height: 50px;
-  width: 80%;
+  width: 85%;
   background-color: #3d85c6;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   border-radius: 8px;
+  margin-top: 20px;
 `;
 
 export const ButtonText = styled.Text`
@@ -48,16 +50,25 @@ export const BoldText = styled.Text`
 
 export const BotaoVoltarHome = styled.TouchableOpacity`
   height: 50px;
-  width: 80%;
+  width: 45%;
   background-color: #14ab94;
   justify-content: center;
   align-items: center;
   border-radius: 8px;
-  margin-top: 40px;  
+  margin-top: 70px;
+  flex-direction: row;  
 `;
 
 export const TextoBotaoVoltar = styled.Text`
   color: white;
   font-size: 18px;
-  font-weight: 600;  
+  font-weight: 600;
+  margin-left: 8px; 
+`;
+
+export const Imagem = styled.Image`
+  width: 70%;
+  height: 200px;
+  align-self: center;
+  margin-bottom: 30px;
 `;
