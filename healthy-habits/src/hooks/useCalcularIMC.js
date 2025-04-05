22c6e14cc7fae96c642340ea1a0c0ba6d377a297
Joao_vitor_oliveira <joaovitor636073@gmail.com>
2025-04-05 20:12:49 -0300

    Criado estrutura de condição para o imc

--- healthy-habits/src/hooks/useCalcularIMC.js
+++ healthy-habits/src/hooks/useCalcularIMC.js
@@ -4,6 +4,7 @@ export function useCalcularIMC() {
   const [peso, setPeso] = useState("");
   const [altura, setAltura] = useState("");
   const [imc, setImc] = useState("");
+  const [classificacao, setClassificacao] = useState("");
 
   function calcularIMC() {
     const pesoValue = parseFloat(peso);
@@ -11,12 +12,36 @@ export function useCalcularIMC() {
 
     if (!pesoValue || !alturaValue) {
       setImc("Dados inválidos");
+      setClassificacao("");
       return;
     }
 
     const imcValue = pesoValue / (alturaValue * alturaValue);
     setImc(imcValue.toFixed(2));
+
+    let classificacao = "";
+    if (imcValue < 18.5) {
+      classificacao = "Baixo peso";
+    } else if (imcValue < 25) {
+      classificacao = "Peso normal";
+    } else if (imcValue < 30) {
+      classificacao = "Excesso de peso";
+    } else if (imcValue < 35) {
+      classificacao = "Obesidade";
+    } else {
+      classificacao = "Obesidade extrema";
+    }
+
+    setClassificacao(classificacao);
   }
 
-  return { peso, altura, imc, setPeso, setAltura, calcularIMC };
+  return {
+    peso,
+    altura,
+    imc,
+    classificacao,
+    setPeso,
+    setAltura,
+    calcularIMC
+  };
 }
