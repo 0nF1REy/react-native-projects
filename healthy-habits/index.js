aa2b0355547fb7aabe7db6b3daf48e262767000e
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-04-05 01:21:26 -0300

    feat: Enhanced "ThemeProvider"

--- healthy-habits/index.js
+++ healthy-habits/index.js
@@ -1,4 +1,14 @@
 import { registerRootComponent } from 'expo';
+import { ThemeProvider } from 'styled-components/native';
+import { colors } from './Theme'; 
 import MainApp from './MainApp';
 
-registerRootComponent(MainApp);
+export default function App() { 
+    return (
+      <ThemeProvider theme={colors}>
+        <MainApp /> {}
+      </ThemeProvider>
+    );
+}
+
+registerRootComponent(App); 
